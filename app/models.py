6075d0dae0d77f81d2a01af33a6500e3f72f58ec
ocafeeoelefante <ocafeeoelefante@gmail.com>
2024-10-02 11:46:22 -0300

    The D is silent

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Carros(models.Model):
+    modelo = models.CharField(max_length=40)
+    marca = models.CharField(max_length=40)
+    ano = models.IntegerField()
+    valor = models.FloatField()
